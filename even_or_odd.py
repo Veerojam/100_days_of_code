b0dcd0bdbf125becfbdb77874f344c53bc5fbbcd
veerojam <mveeroja@gmail.com>
2026-03-06 17:58:56 +0200

    added even_or_odd

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+
+
+nr = int(input("What's the number you wanna' check, m8? "))
+if nr % 2 == 0:
+    print("This number is EVEN!!!!!")
+else:
+    print("This number is ODD!!!!! Lik yo momma!!!")
+    
