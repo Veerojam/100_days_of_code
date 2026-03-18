7acb94a3941781be945f765a8ad6d455a07c97a3
veerojam <mveeroja@gmail.com>
2026-03-18 23:53:19 +0200

    blackjack v2

--- day11_blackjack_v2.py
+++ day11_blackjack_v2.py
@@ -9,39 +9,19 @@ print(ascii)
 #want_to_play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
 player_cards = []
-
-def player_draw():
-    player_cards.append(random.randint(1,11))
-
-def playa_sum():
-    player_sum = 0
-    for card in player_cards:
-        player_sum += card
-    return player_sum
-
-
-
 computer_cards = []
 
-def computer_draw():
-    computer_cards.append(random.randint(1,11))
+
+def draw(list_name):
+    list_name.append(random.randint(1,11))
 
 
-def comp_sum():
-    computer_sum = 0
-    for card in computer_cards:
-        computer_sum += card
-    return computer_sum
+draw(player_cards)
+draw(computer_cards)
 
 
-player_draw()
-computer_draw()
-
-computer_sum = comp_sum()
-player_sum = playa_sum()
-
-print(player_cards)
-print(computer_cards)
+print(f"Your current hand: {player_cards}")
+print(f"Computer's current hand: {computer_cards}")
 
 to_continue = True
 
@@ -53,30 +33,30 @@ while to_continue:
         print(f"Your final hand: {player_cards}")
         print(f"Computer's final hand {computer_cards}")
 
-        if player_sum > computer_sum:
+        if sum(player_cards) > sum(computer_cards):
             print("You win!")
-        elif player_sum == computer_sum:
+        elif sum(player_cards) == sum(computer_cards):
             print("It's a draw!")
         else:
             print("Computer wins!")
         to_continue = False
 
     else:
-        player_draw()
-        player_sum = playa_sum()
+        draw(player_cards)
+        draw(computer_cards)
 
-        if player_sum > 21:
+        print(f"Your current hand: {player_cards}")
+        print(f"Computer's current hand: {computer_cards}")
+
+        if sum(player_cards) > 21:
+            print(f"\nYour final hand: {player_cards}")
+            print(f"Computer's final hand {computer_cards}")
             print("You lose!")
             to_continue = False
 
-        computer_draw()
-        computer_sum = comp_sum()
-
-        print(player_cards)
-        print(player_sum)
-        print(computer_cards)
-        print(computer_sum)
-
-        if computer_sum > 21:
+        if sum(computer_cards) > 21:
+            print(f"\nYour final hand: {player_cards}")
+            print(f"Computer's final hand {computer_cards}")
             print("You win!")
+            to_continue = False
 
