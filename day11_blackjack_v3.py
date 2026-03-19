14b0a42376e05ff248c0fb45478febf3159f5af3
veerojam <mveeroja@gmail.com>
2026-03-19 13:44:08 +0200

    day11 still

--- day11_blackjack_v3.py
+++ day11_blackjack_v3.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 continue_playing = True
 
 while continue_playing:
@@ -12,7 +10,18 @@ while continue_playing:
     else:
 
         ascii = '''
-        SOME ASCIII ARTTT
+                                                                                           
+88          88                       88        88                       88         
+88          88                       88        ""                       88         
+88          88                       88                                 88         
+88,dPPYba,  88 ,adPPYYba,  ,adPPYba, 88   ,d8  88 ,adPPYYba,  ,adPPYba, 88   ,d8   
+88P'    "8a 88 ""     `Y8 a8"     "" 88 ,a8"   88 ""     `Y8 a8"     "" 88 ,a8"    
+88       d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
+88b,   ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"Yba,   
+8Y"Ybbd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
+                                              ,88                                  
+                                            888P"                                  
+
         '''
 
         print(ascii)
