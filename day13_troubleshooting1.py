32efce7a3ccfab27396a6db8a9ef36b2579cd4e5
veerojam <mveeroja@gmail.com>
2026-03-31 22:06:08 +0300

    day13 done

--- day13_troubleshooting1.py
+++ day13_troubleshooting1.py
@@ -55,6 +55,67 @@
 
 ### Exercise 5
 
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+
+# print(f"words_per_page = {word_per_page}")
+# print(f"total words = {total_words}")
+
 ### Exercise 6
 
+# import random
+
+# def mutate(a_list):
+#     b_list = []
+#     new_item = 0
+#     for item in a_list:
+#         new_item = item * 2
+#         new_item += random.randint(1, 3)
+#         new_item = new_item + item
+#         b_list.append(new_item)
+#     print(b_list)
+
+# mutate([1, 2, 3, 5, 8, 13])
+
+
 ### Exercise 7
+
+# def odd_or_even(number):
+#     if number % 2 == 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+
+
+### Exercise 8:
+
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+    
+
+### Exercise 9
+# Target is the number up to which we count
+
+# def fizz_buzz(target):
+#     for number in range(1, target + 1):
+#         if number % 3 == 0 and number % 5 == 0:
+#             print("FizzBuzz")
+#         elif number % 3 == 0:
+#             print("Fizz")
+#         elif number % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(number)
+
+# fizz_buzz(15)
